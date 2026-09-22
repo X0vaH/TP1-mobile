1e3d1c9ce6ff373772bdbecf801ed7e9112904fc
Gothrick <1768974@cstjean.qc.ca>
2026-09-22 16:27:15 -0400

    Commented the appNav for better reusability.

--- app/src/main/java/com/example/tp1/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/tp1/navigation/AppNavigation.kt
@@ -14,32 +14,46 @@ import com.example.tp1.viewmodel.EntrainementsViewModel
 
 object Routes {
     const val ARG_ID = "id"
-    const val LISTE = "liste"
-    const val DETAIL = "detail/{$ARG_ID}"
+    const val LISTE = "liste" // String pour le nom de la route liste.
+    const val DETAIL = "detail/{$ARG_ID}" // Pattern de la route detail
 
-    fun detail(id: Int) = "detail/$id"
+    fun detail(id: Int) = "detail/$id" // Fonction avec le id pour naviger vers la vrai page.
 }
+
 
 @Composable
 fun AppNavigation(
+    // Singleton qui garde le controle de la page actuel et qui peux reculer et avancer a travers les pages
     navController: NavHostController = rememberNavController(),
+    // View model creer une fois ici qui permet de ne pas en avoir plusieurs.
     viewModel: EntrainementsViewModel = viewModel()
 ) {
+    // NavHost est le container de l'ecran et montre l'ecran choisis
+    // StartDestination est l'ecran de depart **alors il faut le changer quand on aura le dashboard**
     NavHost(navController = navController, startDestination = Routes.LISTE) {
+        // Declare l'ecran et lui passe le singleton du viewmodel
         composable(Routes.LISTE) {
             EntrainementsScreen(
                 viewModel = viewModel,
+                // Dit au nav controller de prendre le id de l'entrainement clicker et le passe au details
                 onDetailsClick = { id -> navController.navigate(Routes.detail(id)) }
             )
         }
+        // Declare la route detail.
         composable(
+            // Prends le patern de details des const
             route = Routes.DETAIL,
+            // Dit au nav que le {id} dans le pattern est un int
             arguments = listOf(navArgument(Routes.ARG_ID) { type = NavType.IntType })
         ) { backStackEntry ->
+            // BackstackEntry contient la route ou on est presentement dont le detail/2
+            // On extract le 2 avant de le loader dans la vrai ecran de details
             val id = backStackEntry.arguments?.getInt(Routes.ARG_ID) ?: return@composable
+            // Return l'ecran de details avec le bon id et en lui passant aussi le singleton du viewmodel
             EntrainementDetailScreen(
                 id = id,
                 viewModel = viewModel,
+                // Nous permet de retourner sur le dernier l'ecran avec le navcontroller.
                 onBack = { navController.popBackStack() }
             )
         }
